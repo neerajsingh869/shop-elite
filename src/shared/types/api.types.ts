cb3c98a4005be187ed2fbfe0df350e91f5826999
Neeraj Singh <869neerajsingh@gmail.com>
2026-03-13 18:32:12 +0530

    feat: Add types for api responses & product specific details

---
+++ src/shared/types/api.types.ts
@@ -0,0 +1,43 @@
+export interface Category {
+  slug: string;
+  name: string;
+  url: string;
+}
+
+export interface Review {
+  rating: number;
+  comment: string;
+  date: string;
+  reviewerName: string;
+  reviewerEmail: string;
+}
+
+export interface Product {
+  id: number;
+  title: string;
+  category: string;
+  price: number;
+  discountPercentage: number;
+  rating: number;
+  stock: number;
+  brand?: string;
+  thumbnail: string;
+}
+
+export interface ProductDetail extends Product {
+  description: string;
+  images: string[];
+  tags: string[];
+  sku: string;
+  warrantyInformation: string;
+  shippingInformation: string;
+  availabilityStatus: string;
+  reviews: Review[];
+}
+
+export interface ProductResponse {
+  products: Product[],
+  total: number;
+  skip: number;
+  limit: number;
+}
