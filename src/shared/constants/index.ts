33ad78c93111510ce9f64815af0ab1f5ca8f8ff1
Neeraj Singh <neerajsingh@Divine.local>
2026-03-14 16:17:03 +0530

    feat: Create constant file for routes

---
+++ src/shared/constants/index.ts
@@ -0,0 +1,19 @@
+export const BASE_URL = "https://dummyjson.com";
+
+function createProductSlug(title: string): string {
+  const productSlug = title
+    .toLowerCase()
+    .trim()
+    .replace(/[^a-z0-9\s-]/g, "")
+    .replace(/\s+/g, "-")
+    .replace(/-+/g, "-");
+
+  return productSlug;
+}
+
+export const ROUTES = {
+  home: "/",
+  category: (categorySlug: string) => `/${categorySlug}`,
+  product: (categorySlug: string, productId: number, title: string) =>
+    `/${categorySlug}/${productId}/${createProductSlug(title)}`,
+} as const;
