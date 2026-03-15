f0a905c18c4a695ef57af831ca7f07bfc1442e31
Neeraj Singh <neerajsingh@Divine.local>
2026-03-15 07:03:43 +0530

    feat: Create routes and suspense fallback component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
+import { lazy, Suspense } from "react";
+import { BrowserRouter, Route, Routes } from "react-router";
+
 import "./App.css";
+import Header from "./shared/components/Header";
+import SuspenseFallback from "./shared/components/SuspenseFallback";
+
+const NotFoundPage = lazy(() => import("./pages/NotFoundPage"))
+const ProductDetailPage = lazy(() => import("./pages/ProductDetailPage"))
+const ProductListingPage = lazy(() => import("./pages/ProductListingPage"))
+const HomePage = lazy(() => import("./pages/HomePage"))
 
 function App() {
-  return <div>hello world</div>;
+  return (
+    <BrowserRouter>
+      <div>
+        {/* Lazy loaded components need Suspense to handle crash */}
+        <Suspense fallback={<SuspenseFallback />}>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path=":categorySlug" element={<ProductListingPage />} />
+            <Route path=":categorySlug/:productId/:productSlug" element={<ProductDetailPage />} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+        </Suspense>
+      </div>
+    </BrowserRouter>
+  )
 }
 
 export default App;
