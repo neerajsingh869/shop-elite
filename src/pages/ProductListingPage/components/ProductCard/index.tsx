f238c7c256321b1d05c04714f1eb0942dbf676e9
Neeraj Singh <869neerajsingh@gmail.com>
2026-03-16 17:06:28 +0530

    feat: Build product listing page

---
+++ src/pages/ProductListingPage/components/ProductCard/index.tsx
@@ -0,0 +1,44 @@
+import { Star } from "lucide-react";
+import type { Product } from "../../../../shared/types/api.types";
+
+interface ProductCardProps {
+  product: Product;
+}
+
+function ProductCard({ product }: ProductCardProps) {
+  return (
+    <article className="group bg-zinc-950 border-zinc-800 border rounded-xl transition duration-300 hover:border-yellow-700/50 hover:-translate-y-1 overflow-hidden">
+      <div className="relative aspect-square bg-neutral-900 rounded-t-xl">
+        <img
+          className="w-full h-full object-contain transition-transform duration-500 group-hover:scale-105 p-3"
+          src={product.thumbnail}
+          alt={product.title}
+          loading="lazy"
+        />
+        {product.discountPercentage > 10 && (
+          <span className="absolute top-2 left-2 text-xs font-bold text-black bg-yellow-500 rounded py-0.5 px-2">
+            -{Math.round(product.discountPercentage)}%
+          </span>
+        )}
+      </div>
+      <div className="p-3">
+        <p className="text-xs mb-2 line-clamp-2">{product.title}</p>
+        <div className="flex justify-between items-end">
+          <span className="text-sm font-bold text-yellow-400">
+            ${product.price}
+          </span>
+          <span className="text-xs text-zinc-500 flex items-center gap-1">
+            <Star
+              size={10}
+              fill="oklch(79.5% 0.184 86.047)"
+              color="oklch(79.5% 0.184 86.047)"
+            />{" "}
+            {product.rating}
+          </span>
+        </div>
+      </div>
+    </article>
+  );
+}
+
+export default ProductCard;
