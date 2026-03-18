4f9ffacbb555e06d5e483e831e23f359e4c0037c
Neeraj Singh <869neerajsingh@gmail.com>
2026-03-18 16:20:07 +0530

    feat: Add back button link on product detail page

--- src/pages/ProductDetailPage/index.tsx
+++ src/pages/ProductDetailPage/index.tsx
@@ -9,6 +9,8 @@ import ImageGallery from "./components/ImageGallery";
 import AboutProduct from "./components/AboutProduct";
 import Reviews from "./components/Reviews";
 import ProductDetailPageSkeleton from "./skeleton";
+import BackButton from "../../shared/components/ui/BackButton";
+import getCategoryName from "../../shared/utils/getCategoryName";
 
 interface PageParams extends Record<string, string> {
   categorySlug: string;
@@ -26,9 +28,15 @@ function ProductDetailPage() {
   );
   const { topRef } = useScrollToTop(loading);
 
+  const categoryName = getCategoryName(categorySlug);
+
   return (
     <>
       <div className="absolute top-0" ref={topRef}></div>
+      <BackButton
+        to={categorySlug ? ROUTES.category(categorySlug) : ROUTES.home}
+        label={`Back to ${categoryName}`}
+      />
       {loading ? (
         <ProductDetailPageSkeleton />
       ) : error ? (
@@ -42,10 +50,12 @@ function ProductDetailPage() {
               <div className="flex flex-col gap-5 items-start">
                 {/* Link to go to product listing page for the category */}
                 <Link
-                  to={ROUTES.category(categorySlug!)}
+                  to={
+                    categorySlug ? ROUTES.category(categorySlug) : ROUTES.home
+                  }
                   className="border border-yellow-700 rounded-full px-3 py-1 uppercase text-xs font-semibold tracking-wide text-yellow-500 bg-yellow-900/20"
                 >
-                  {data.category}
+                  {categoryName}
                 </Link>
                 {/* All the important product information */}
                 <AboutProduct data={data} />
