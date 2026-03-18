bf4e5c432f123a487bd91c08070ab9c5b680babe
Neeraj Singh <869neerajsingh@gmail.com>
2026-03-18 16:44:38 +0530

    feat: Build basic header

--- src/shared/components/Header/index.tsx
+++ src/shared/components/Header/index.tsx
@@ -1,5 +1,25 @@
+import { Link } from "react-router";
+import { Search, ShoppingCart, User } from "lucide-react";
+
+import { ROUTES } from "../../constants";
+
 function Header() {
-  return <div>Header</div>
+  return (
+    <>
+      <header className="sticky p-6 border-b border-b-zinc-800">
+        <div className="max-w-7xl flex justify-between items-center mx-auto">
+          <Link to={ROUTES.home} className="text-xl text-yellow-500 font-bold">
+            ShopElite
+          </Link>
+          <div className="flex gap-4 sm:gap-6 md:gap-8 items-center justify-between">
+            <Search className="cursor-pointer" size={20} />
+            <ShoppingCart className="cursor-pointer" size={20} />
+            <User className="cursor-pointer" size={20} />
+          </div>
+        </div>
+      </header>
+    </>
+  );
 }
 
 export default Header;
