73afce14dde5ca4948d47fea3178961a161569be
Neeraj Singh <869neerajsingh@gmail.com>
2026-03-23 22:07:52 +0530

    style: Create skeleton for back button

---
+++ src/shared/components/ui/BackButtonSkeleton.tsx
@@ -0,0 +1,7 @@
+function BackButtonSkeleton() {
+  return (
+    <div className="bg-zinc-800 h-8 w-32 mb-6 rounded-lg border-zinc-800 animate-pulse"></div>
+  );
+}
+
+export default BackButtonSkeleton;
