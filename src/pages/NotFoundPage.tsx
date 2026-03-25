fd873d37c4f142acf15034f30ae9d4e3c1c20600
Neeraj Singh <869neerajsingh@gmail.com>
2026-03-25 19:30:05 +0530

    style: Reduce text size

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -5,10 +5,10 @@ function NotFoundPage() {
   return (
     <div className="flex flex-col justify-center items-center min-h-[70vh] gap-4">
       <div className="flex flex-col items-center">
-        <span className="text-5xl md:text-6xl lg:text-8xl font-bold text-zinc-100">
+        <span className="text-4xl md:text-5xl lg:text-6xl font-bold text-zinc-100">
           404
         </span>
-        <span className="text-zinc-500 text-lg lg:text-xl text-center">
+        <span className="text-zinc-500 text-base lg:text-lg text-center">
           This page could not be found.
         </span>
       </div>
