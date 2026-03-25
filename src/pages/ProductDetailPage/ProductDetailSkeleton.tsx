b655b9e59b99d45badc6f0dea5c66f37af8b1846
Neeraj Singh <869neerajsingh@gmail.com>
2026-03-25 19:45:41 +0530

    feat: Build and use product not found page in product detail page

--- src/pages/ProductDetailPage/ProductDetailSkeleton.tsx
+++ src/pages/ProductDetailPage/ProductDetailSkeleton.tsx
@@ -1,10 +1,12 @@
 import ImageGallarySkeleton from "./components/ImageGallery/ImageGallerySkeleton";
 import AboutProductSkeleton from "./components/AboutProduct/AboutProductSkeleton";
 import ReviewsSkeleton from "./components/Reviews/ReviewsSkeleton";
+import BackButtonSkeleton from "../../shared/components/ui/BackButtonSkeleton";
 
 function ProductDetailPageSkeleton() {
   return (
     <>
+      <BackButtonSkeleton />
       <div className="grid grid-cols-1 md:grid-cols-2 gap-12">
         <ImageGallarySkeleton />
         <div className="flex flex-col gap-5 items-start">
