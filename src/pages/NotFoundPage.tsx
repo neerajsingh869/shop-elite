6da938d83eddf3ad5573f8580f900ede942c7b90
Neeraj Singh <869neerajsingh@gmail.com>
2026-03-25 17:46:09 +0530

    style: Add styles to not found page

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,5 +1,20 @@
+import { ROUTES } from "../shared/constants";
+import BackButton from "../shared/components/ui/BackButton";
+
 function NotFoundPage() {
-  return <div>Not found page</div>;
+  return (
+    <div className="flex flex-col justify-center items-center min-h-[70vh] gap-4">
+      <div className="flex flex-col items-center">
+        <span className="text-5xl md:text-6xl lg:text-8xl font-bold text-zinc-100">
+          404
+        </span>
+        <span className="text-zinc-500 text-lg lg:text-xl text-center">
+          This page could not be found.
+        </span>
+      </div>
+      <BackButton to={ROUTES.home} label="Go to Home" />
+    </div>
+  );
 }
 
 export default NotFoundPage;
