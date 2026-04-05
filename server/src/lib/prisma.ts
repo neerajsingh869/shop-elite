4be033f7d4012f30c748fd9cee5cf3360b6bfba0
Neeraj Singh <869neerajsingh@gmail.com>
2026-04-05 15:06:33 +0530

    server/config: Setup prisma using node-postgresql adapter

---
+++ server/src/lib/prisma.ts
@@ -0,0 +1,10 @@
+import "dotenv/config"; // Loads .env file in process.env
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "../generated/prisma/client.js";
+
+const connectionString = `${process.env.DATABASE_URL}`;
+
+const adapter = new PrismaPg({ connectionString });
+const prisma = new PrismaClient({ adapter });
+
+export { prisma };
