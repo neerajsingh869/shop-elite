2f60b197a45f32ce3a68baceeeb03d9ebaa1fae2
Neeraj Singh <869neerajsingh@gmail.com>
2026-04-20 18:28:26 +0530

    server/feat: Create product filters type

---
+++ server/src/features/products/product.types.ts
@@ -0,0 +1,11 @@
+interface ProductFilters {
+  keyword?: string;
+  category?: string;
+  brand?: string;
+  minPrice?: number;
+  maxPrice?: number;
+  minDiscount?: number;
+  minRating?: number;
+  availabilityStatus?: string;
+  sortBy?: "price_asc" | "price_desc" | "rating_desc" | "discount_desc"
+}
