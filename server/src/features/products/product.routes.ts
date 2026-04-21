b4ac9c747b668b658b10ea616c14c1aec66e86af
Neeraj Singh <869neerajsingh@gmail.com>
2026-04-21 22:39:28 +0530

    server/feat: Refactor product controllers and routes

--- server/src/features/products/product.routes.ts
+++ server/src/features/products/product.routes.ts
@@ -4,7 +4,7 @@ import * as productController from "./product.controller.js";
 
 const router = Router();
 
-router.get("/search", productController.searchProducts);
-router.post("/llm-search", productController.searchProductsUsingLLM);
+router.get("/search", productController.searchProductsHandler);
+router.post("/llm-search", productController.llmSearchHandler);
 
 export default router;
