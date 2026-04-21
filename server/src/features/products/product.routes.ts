e1f7e0ac305ca6c060e93643adc66c4fc0eb68d3
Neeraj Singh <869neerajsingh@gmail.com>
2026-04-21 21:33:53 +0530

    server/feat: Build LLM endpoint to handle search using llm feature

--- server/src/features/products/product.routes.ts
+++ server/src/features/products/product.routes.ts
@@ -5,5 +5,6 @@ import * as productController from "./product.controller.js";
 const router = Router();
 
 router.get("/search", productController.searchProducts);
+router.post("/llm-search", productController.searchProductsUsingLLM);
 
 export default router;
