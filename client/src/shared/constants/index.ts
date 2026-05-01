6831f32d0792594f49257e0d925a7fec509f161e
Neeraj Singh <869neerajsingh@gmail.com>
2026-05-01 11:40:09 +0530

    client/feat: Add page and limit query params in the api to get products for a choosen category

--- client/src/shared/constants/index.ts
+++ client/src/shared/constants/index.ts
@@ -3,8 +3,13 @@ import createProductSlug from "../utils/createProductSlug";
 export const BASE_URL = import.meta.env.VITE_API_URL;
 
 export const GET_CATEGORIES_URL = `${BASE_URL}/api/products/categories`;
-export const GET_PRODUCTS_BY_CATEGORY_URL = (categorySlug: string) =>
-  `${BASE_URL}/api/products/search?category=${categorySlug}`;
+// api to get paginated response of products for the specified category
+export const GET_PRODUCTS_BY_CATEGORY_URL = (
+  categorySlug: string,
+  page: number,
+  limit: number,
+) =>
+  `${BASE_URL}/api/products/search?category=${categorySlug}&page=${page}&limit=${limit}`;
 export const GET_PRODUCT_URL = (productId: number) =>
   `${BASE_URL}/api/products/${productId}`;
 
