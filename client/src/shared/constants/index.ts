c5f7a2d4613a7b534895d6a3228feeca9fe276f9
Neeraj Singh <869neerajsingh@gmail.com>
2026-05-02 15:59:57 +0530

    client/feat: Add functions to build category metadata and search url

--- client/src/shared/constants/index.ts
+++ client/src/shared/constants/index.ts
@@ -1,3 +1,4 @@
+import type { ProductFilters } from "../../features/products/product.types";
 import createProductSlug from "../utils/createProductSlug";
 
 export const BASE_URL = import.meta.env.VITE_API_URL;
@@ -7,11 +8,31 @@ export const GET_CATEGORIES_URL = `${BASE_URL}/api/products/categories`;
 export const GET_PRODUCTS_BY_CATEGORY_URL = (
   categorySlug: string,
   page: number,
-  limit: number,
-) =>
-  `${BASE_URL}/api/products/search?category=${categorySlug}&page=${page}&limit=${limit}`;
+) => `${BASE_URL}/api/products/search?category=${categorySlug}&page=${page}`;
 export const GET_PRODUCT_URL = (productId: number) =>
   `${BASE_URL}/api/products/${productId}`;
+
+export const buildSearchURL = (filters: ProductFilters, page?: number) => {
+  const params = new URLSearchParams();
+
+  if (filters.keyword) params.set("keyword", filters.keyword);
+  if (filters.category) params.set("category", filters.category);
+  if (filters.brand) params.set("brand", filters.brand);
+  if (filters.minPrice) params.set("minPrice", String(filters.minPrice));
+  if (filters.maxPrice) params.set("maxPrice", String(filters.maxPrice));
+  if (filters.minDiscount)
+    params.set("minDiscount", String(filters.minDiscount));
+  if (filters.minRating) params.set("minRating", String(filters.minRating));
+  if (filters.availabilityStatus)
+    params.set("availabilityStatus", filters.availabilityStatus);
+  if (filters.sortBy) params.set("sortBy", filters.sortBy);
+  if (page) params.set("page", String(page));
+
+  return `${BASE_URL}/api/products/search?${params.toString()}`;
+};
+
+export const buildCategoryMetadataURL = (category: string) =>
+  `${BASE_URL}/api/products/category-metadata?category=${category}`;
 
 export const ROUTES = {
   home: "/",
