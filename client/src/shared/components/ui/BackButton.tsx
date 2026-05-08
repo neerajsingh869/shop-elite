e62d9a9d58d54593493f81e42de31017e05e554e
Neeraj Singh <869neerajsingh@gmail.com>
2026-05-08 20:21:45 +0530

    client/feat: Add header on all products page along with handling different states

--- client/src/shared/components/ui/BackButton.tsx
+++ client/src/shared/components/ui/BackButton.tsx
@@ -1,19 +1,42 @@
 import { ArrowLeft } from "lucide-react";
-import { Link } from "react-router";
+import { Link, useLocation, useNavigate } from "react-router";
 
 interface BackButtonProps {
-  to: string;
+  to?: string;
   label: string;
 }
 
 function BackButton({ to, label }: BackButtonProps) {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  function handleBack() {
+    if (location.key === "default") {
+      navigate("/", { replace: true });
+    } else {
+      navigate(-1);
+    }
+  }
+
   return (
-    <Link
-      to={to}
-      className="text-sm text-zinc-400 border bg-zinc-950 border-zinc-800 px-4 py-2 rounded-lg mb-6 inline-flex items-center justify-center gap-1 transition-all hover:text-yellow-400 hover:border-yellow-600"
-    >
-      <ArrowLeft size={16} strokeWidth={1.5} /> {label}
-    </Link>
+    <>
+      {to ? (
+        <Link
+          to={to}
+          className="text-sm text-zinc-400 border bg-zinc-950 border-zinc-800 px-4 py-2 rounded-lg mb-6 inline-flex items-center justify-center gap-1 transition-all hover:text-yellow-400 hover:border-yellow-600"
+        >
+          <ArrowLeft size={16} strokeWidth={1.5} /> {label}
+        </Link>
+      ) : (
+        <button
+          onClick={handleBack}
+          className="text-sm text-zinc-400 border bg-zinc-950 border-zinc-800 px-4 py-2 rounded-lg mb-6 inline-flex items-center justify-center gap-1 transition-all hover:text-yellow-400 hover:border-yellow-600"
+        >
+          {" "}
+          <ArrowLeft size={16} strokeWidth={1.5} /> {label}
+        </button>
+      )}
+    </>
   );
 }
 
