9ce3b776b6888416b0c3ea2d137345e7c64c1b84
Neeraj Singh <869neerajsingh@gmail.com>
2026-05-28 18:12:01 +0530

    client/feat: Write all api calls for auth in single file

---
+++ client/src/features/auth/api.ts
@@ -0,0 +1,35 @@
+import type { AuthResponse } from "./types";
+import { api } from "../../shared/lib/axios";
+
+// api to register user
+export async function registerApi(data: {
+  name: string;
+  email: string;
+  password: string;
+}) {
+  const res = await api.post<AuthResponse>("/api/register", data);
+  return res.data;
+}
+
+// api to login user
+export async function loginApi(data: { email: string; password: string }) {
+  const res = await api.post<AuthResponse>("/api/login", data);
+  return res.data;
+}
+
+// api to refresh the access token on app startup to restore session
+export async function refreshApi() {
+  const res = await api.post<AuthResponse>("/api/refresh");
+  return res.data;
+}
+
+// api to logout the user and revoke the refresh token
+export async function logoutApi() {
+  await api.post("/api/logout");
+}
+
+// api for google login/register
+export async function googleAuthApi(accessToken: string) {
+  const res = await api.post<AuthResponse>("/api/google", { accessToken });
+  return res.data;
+}
