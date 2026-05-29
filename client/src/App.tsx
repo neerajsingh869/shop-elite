bd8b33fcd28f0ecfa3815253e83d6fef5da33d8d
Neeraj Singh <869neerajsingh@gmail.com>
2026-05-29 12:42:23 +0530

    client/feat: Create protected route, update app routes, wrap app in redux provider & use hook to restore session on page load or refresh

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,33 @@
 import { BrowserRouter } from "react-router";
 
 import "./App.css";
-import Header from "./shared/components/Header/Header";
+import { store } from "./store";
+import { Provider } from "react-redux";
 import AppRoutes from "./routes/routes";
+import { useInitAuth } from "./features/auth/hooks";
+import Header from "./shared/components/Header/Header";
+
+function AppContent() {
+  useInitAuth();
+
+  return (
+    <div className="min-h-screen bg-zinc-950 text-zinc-100">
+      {/* Header is outside Routes so renders on every page */}
+      <Header />
+      <main className="max-w-7xl mx-auto p-6 md:p-8">
+        <AppRoutes />
+      </main>
+    </div>
+  );
+}
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="min-h-screen bg-zinc-950 text-zinc-100">
-        {/* Header is outside Routes so renders on every page */}
-        <Header />
-        <main className="max-w-7xl mx-auto p-6 md:p-8">
-          <AppRoutes />
-        </main>
-      </div>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <AppContent />
+      </BrowserRouter>
+    </Provider>
   );
 }
 
