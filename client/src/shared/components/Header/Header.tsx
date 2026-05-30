be550bee5250a0e7f4950b29a362ab26df3665a8
Neeraj Singh <869neerajsingh@gmail.com>
2026-05-30 19:35:49 +0530

    client/feat: Add user menu and signin buttons according to user authentication status

--- client/src/shared/components/Header/Header.tsx
+++ client/src/shared/components/Header/Header.tsx
@@ -1,10 +1,24 @@
+import { useState } from "react";
 import { Link } from "react-router";
-import { ShoppingCart, User } from "lucide-react";
+import { ChevronDown, LogOut, ShoppingCart, User } from "lucide-react";
 
 import { ROUTES } from "../../constants";
+import {
+  selectIsAuthenticated,
+  selectUser,
+} from "../../../features/auth/authSelectors";
 import SearchBar from "../SearchBar/SearchBar";
+import { useAppSelector } from "../../../store/hook";
+import { useAuth } from "../../../features/auth/hooks";
 
 function Header() {
+  const isAuthenticated = useAppSelector(selectIsAuthenticated);
+  const user = useAppSelector(selectUser);
+
+  const { logout } = useAuth();
+
+  const [showUserMenu, setShowUserMenu] = useState<boolean>(false);
+
   return (
     <>
       <header className="border-b border-b-zinc-800">
@@ -15,7 +29,62 @@ function Header() {
           <div className="flex gap-4 sm:gap-6 md:gap-8 items-center justify-between">
             <SearchBar />
             <ShoppingCart className="cursor-pointer" size={20} />
-            <User className="cursor-pointer" size={20} />
+            {isAuthenticated && user ? (
+              <div className="relative">
+                <button
+                  onClick={() => setShowUserMenu((v) => !v)}
+                  className="cursor-pointer flex items-center justify-center gap-1"
+                >
+                  {user?.avatarUrl ? (
+                    <img
+                      src={user.avatarUrl}
+                      className="w-7 h-7 rounded-full object-cover"
+                    />
+                  ) : (
+                    <div className="w-7 h-7 rounded-full bg-yellow-500 flex items-center justify-center uppercase text-zinc-950 font-bold text-sm">
+                      {user.name.charAt(0)}
+                    </div>
+                  )}
+                  <ChevronDown
+                    size={15}
+                    className={`text-zinc-500 transition-colors ${showUserMenu ? "rotate-180" : ""}`}
+                  />
+                </button>
+
+                {showUserMenu && (
+                  <div className="border absolute right-0 top-full mt-2 w-52 rounded-xl border-zinc-800 bg-zinc-900 shadow-xl overflow-hidden z-50">
+                    <div className="border-b border-zinc-800 px-4 py-3">
+                      <p className="text-sm text-zinc-100 font-medium truncate">
+                        {user.name}
+                      </p>
+                      <p className="text-xs text-zinc-500 truncate">
+                        {user.email}
+                      </p>
+                    </div>
+                    <div className="px-4 py-3">
+                      <button
+                        onClick={() => {
+                          setShowUserMenu(false);
+                          logout();
+                        }}
+                        className="text-red-400 text-sm flex gap-3 items-center cursor-pointer"
+                      >
+                        <LogOut size={15} />
+                        <span>Log out</span>
+                      </button>
+                    </div>
+                  </div>
+                )}
+              </div>
+            ) : (
+              <Link
+                to="/login"
+                className="flex items-center gap-1 text-sm text-zinc-400 hover:text-zinc-100 transition-colors"
+              >
+                <User className="cursor-pointer" size={18} />
+                <span className="hidden sm:block">Sign in</span>
+              </Link>
+            )}
           </div>
         </div>
       </header>
