7dc80184b40097f0fbcd6a2f9530d0c25c41f9de
Neeraj Singh <869neerajsingh@gmail.com>
2026-06-01 10:16:06 +0530

    client/fix: Number conversion & missing skeleton in reviews skeleton component

--- client/src/pages/ProductDetailPage/components/Reviews/ReviewsSkeleton.tsx
+++ client/src/pages/ProductDetailPage/components/Reviews/ReviewsSkeleton.tsx
@@ -1,13 +1,13 @@
 import ReviewGridSkeleton from "./components/ReviewGrid/ReviewGridSkeleton";
 import ReviewsMetadataSkeleton from "./components/ReviewsMetadata/ReviewsMetadataSkeleton";
-import WriteReview from "./components/WriteReview/WriteReview";
+import WriteReviewSkeleton from "./components/WriteReview/WriteReviewSkeleton";
 
 function ReviewsSkeleton() {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 gap-12 mt-16">
       <div className="flex flex-col gap-8">
         <ReviewsMetadataSkeleton />
-        <WriteReview />
+        <WriteReviewSkeleton />
       </div>
       <ReviewGridSkeleton />
     </div>
---
+++ client/src/pages/ProductDetailPage/components/Reviews/components/WriteReview/WriteReviewSkeleton.tsx
@@ -0,0 +1,6 @@
+function WriteReviewSkeleton() {
+  // TODO
+  return <div>Write review skeleton</div>;
+}
+
+export default WriteReviewSkeleton;
