351bededaf2d98ca8f5cc3d6c9f1158daf644244
Neeraj Singh <869neerajsingh@gmail.com>
2026-06-01 11:08:28 +0530

    client/chore: Update axios to use 3000 port for local server communication

--- client/src/shared/lib/axios.ts
+++ client/src/shared/lib/axios.ts
@@ -5,7 +5,7 @@ import type { AuthResponse } from "../../features/auth/types";
 import { clearAuth, setAuth } from "../../features/auth/authSlice";
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL ?? "http://localhost:4000",
+  baseURL: import.meta.env.VITE_API_URL ?? "http://localhost:3000",
   // tell browser to send httpOnly cookie with every request
   // equivalent to credentials: "include" in fetch requests
   withCredentials: true,
