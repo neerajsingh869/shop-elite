96b73a4d98cadad8ee99569fb427961f602d5741
Neeraj Singh <869neerajsingh@gmail.com>
2026-06-02 16:33:34 +0530

    client/cart: Write logic to preload global cart state from localstorage & attach listener middleware

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -2,11 +2,23 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import { authReducer } from "../features/auth/authSlice";
 import { cartReducer } from "../features/cart/cartSlice";
+import { loadCart } from "../features/cart/cartPersistence";
+import { listenerMiddleware } from "../features/cart/cartMiddleware";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     cart: cartReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().prepend(listenerMiddleware.middleware), // listeners should run first, that's why prepend
+  devTools: import.meta.env.VITE_NODE_ENV !== "production",
+  preloadedState: {
+    cart: {
+      // loadCart() reads from localstorage at store cration time before any component mounts
+      items: loadCart(),
+      isOpen: false,
+    },
   },
 });
 
