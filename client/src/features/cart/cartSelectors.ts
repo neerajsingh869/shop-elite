1a3d812a27df987d3746877f41b5c0c3d716ec86
Neeraj Singh <869neerajsingh@gmail.com>
2026-06-02 11:02:17 +0530

    client/cart: use createSelector for memoization benefits

--- client/src/features/cart/cartSelectors.ts
+++ client/src/features/cart/cartSelectors.ts
@@ -1,8 +1,15 @@
+import { createSelector } from "@reduxjs/toolkit";
+
 import type { RootState } from "../../store";
 
 export const selectCartItems = (state: RootState) => state.cart.items;
 export const selectIsCartOpen = (state: RootState) => state.cart.isOpen;
-export const selectCartCount = (state: RootState) =>
-  state.cart.items.reduce((count, item) => count + item.quantity, 0);
-export const selectCartTotal = (state: RootState) =>
-  state.cart.items.reduce((total, item) => total + item.price * item.quantity, 0);
+
+// use createSelector for memoization benefits
+export const selectCartCount = createSelector(
+  selectCartItems, // output becomes input for below function
+  (items) => items.reduce((count, item) => count + item.quantity, 0),
+);
+export const selectCartTotal = createSelector(selectCartItems, (items) =>
+  items.reduce((total, item) => total + item.price * item.quantity, 0),
+);
